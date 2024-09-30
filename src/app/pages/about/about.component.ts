b358945d3c68903451b859595f43b1970d40fede
wikki-devops <wikkizofficial@gmail.com>
2024-09-30 12:03:19 +0530

    second

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
   pagetitle = 'Who we are?';
+  needs: any[] = [
+    { icons: '', title: 'Green IVF' },
+    { icons: '1.webp', title: 'Mini IVF' },
+    { icons: '3.webp', title: 'IVF' },
+    { icons: '2.webp', title: 'IUF' },
+    { icons: '4.webp', title: 'Pre-Genetic Diagnosis' },
+    { icons: '4.webp', title: 'ICSI Assisted Hatching' },
+    { icons: '4.webp', title: 'Surrogacy programmes' },
+    { icons: '4.webp', title: 'TESE' },
+    { icons: '4.webp', title: 'Donor programmes' },
+
+  ];
 }
